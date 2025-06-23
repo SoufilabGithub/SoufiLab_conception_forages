16d741d75c278d30c63bbb8e241a4dc4a944e7c5
SoufilabGithub <167484266+SoufilabGithub@users.noreply.github.com>
2025-06-23 04:47:46 -0400

    Add files via upload

---
+++ section_I_exploration_eaux_souterraines.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Titre de la section
+st.markdown("## I. EXPLORATION DES EAUX SOUTERRAINES")
+
+# Message d'information
+st.info("Cette partie sera développée dans le futur avec des technologies de reconnaissance des eaux souterraines.")
